dd813bdb488bc80e89c7de8f8947ff3f6eca73a0
Zeev Rotshtein <zeevro@gmail.com>
2020-11-02 14:18:36 +0200

    Minor cleanups in service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -12,15 +12,13 @@ var filesToCache = [
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open(cacheName).then(cache=>cache.addAll(filesToCache))
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
   );
 });
 
 /* Serve cached content when offline */
 self.addEventListener('fetch', function(e) {
   e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
