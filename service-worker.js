315834a519329b07b2df910ecf2cbd7461284927
Zeev Rotshtein <zeevro@gmail.com>
2020-11-02 17:02:27 +0200

    Fix service-worker.js to always update from network when possible

--- service-worker.js
+++ service-worker.js
@@ -12,13 +12,17 @@ var filesToCache = [
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+    caches.open(cacheName).then(function (cache) {
+      cache.addAll(filesToCache)
+    })
   );
 });
 
 /* Serve cached content when offline */
 self.addEventListener('fetch', function(e) {
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    fetch(e.request).catch(function() {
+      return caches.match(e.request);
+    })
   );
 });
