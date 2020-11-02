4394b579a8f3f438c5005c08cdeb37791eab5fc7
Zeev Rotshtein <zeevro@gmail.com>
2020-11-02 14:54:51 +0200

    Basic core functionality achieved!

--- app.js
+++ app.js
@@ -5,50 +5,75 @@ window.addEventListener("load", () => {
 });
 
 var points = [];
+var currentCoords = null;
+var currentAngle = 0;
+var arrow = document.getElementById('arrow');
+var waypointSelect = document.getElementById('waypoint');
 
 fetch('points.json')
   .then(response => response.json())
-  .then(json => { points = json; });
+  .then(json => {
+    points = json;
 
-var lastAngle = 0;
-var arrow = document.getElementById('arrow');
+    points.forEach((point, i) => {
+      var opt = document.createElement('option');
+      opt.innerHTML = point.name;
+      opt.value = i;
+      waypointSelect.appendChild(opt);
+    });
+  });
 
 function rotateArrow(deg) {
   arrow.setAttribute('transform', 'rotate(' + deg + ' 100 100)')
 }
 
 function newAngle(deg) {
-  console.log('newAngle(' + deg + ') lastAngle=' + lastAngle);
-  if (lastAngle > 180 && deg < 180) {
-    arrow.classList.remove('transition');
-    rotateArrow(lastAngle - 360);
-    setTimeout(()=> {
-      arrow.classList.add('transition')
-      rotateArrow(deg);
-    } , 1);
-  } else if (lastAngle < 180 && deg > 180) {
-    arrow.classList.remove('transition');
-    rotateArrow(lastAngle + 360);
-    setTimeout(()=> {
-      arrow.classList.add('transition')
-      rotateArrow(deg);
-    } , 1);
-  } else {
-    rotateArrow(deg);
-  }
-  lastAngle = deg;
+  console.log('newAngle(' + deg + ') currentAngle=' + currentAngle);
+  // if (currentAngle > 180 && deg < 180) {
+  //   arrow.classList.remove('transition');
+  //   rotateArrow(currentAngle - 360);
+  //   setTimeout(()=> {
+  //     arrow.classList.add('transition')
+  //     rotateArrow(deg);
+  //   } , 1);
+  // } else if (currentAngle < 180 && deg > 180) {
+  //   arrow.classList.remove('transition');
+  //   rotateArrow(currentAngle + 360);
+  //   setTimeout(()=> {
+  //     arrow.classList.add('transition')
+  //     rotateArrow(deg);
+  //   } , 1);
+  // } else {
+  //   rotateArrow(deg);
+  // }
+  rotateArrow(deg);
+  currentAngle = deg;
 }
 
+function updateNavData() {
+  if (!waypointSelect.selectedOptions.length) return;
+  var waypoint = points[waypointSelect.selectedOptions[0].value];
+  var bearing = GreatCircle.bearing(currentCoords.latitude, currentCoords.longitude, waypoint.lat, waypoint.lon) - currentCoords.heading;
+  var distance = GreatCircle.distance(currentCoords.latitude, currentCoords.longitude, waypoint.lat, waypoint.lon);
+  document.getElementById('bearing').innerHTML = bearing.toFixed(2);
+  document.getElementById('distance').innerHTML = distance.toFixed(2) + ' km';
+  newAngle(bearing);
+}
+
+waypointSelect.addEventListener('change', updateNavData);
+
 // setInterval(() => {
-//   newAngle((lastAngle + 360 + 30) % 360);
+//   newAngle((currentAngle + 360 + 30) % 360);
 // }, 500);
 
 navigator.geolocation.watchPosition(
   position => {
-    console.log(position.coords.latitude, position.coords.longitude, position.coords.speed, position.coords.heading);
-    document.getElementById('location').innerHTML = position.coords.latitude + ', ' + position.coords.longitude
-    document.getElementById('speed').innerHTML = position.coords.speed
-    document.getElementById('heading').innerHTML = position.coords.heading
+    currentCoords = position.coords;
+    console.log(currentCoords.latitude, currentCoords.longitude, currentCoords.speed, currentCoords.heading);
+    document.getElementById('location').innerHTML = currentCoords.latitude.toFixed(6) + ', ' + currentCoords.longitude.toFixed(6);
+    document.getElementById('speed').innerHTML = currentCoords.speed.toFixed(2);
+    document.getElementById('heading').innerHTML = currentCoords.heading.toFixed(2);
+    updateNavData();
     var angle = position.coords.heading;
     if (angle === null || angle === NaN || angle === undefined) return;
     newAngle(angle);
