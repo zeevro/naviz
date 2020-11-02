2cce9087f1ea2afa96859f8e28888f138b124f4f
Zeev Rotshtein <zeevro@gmail.com>
2020-11-02 17:20:52 +0200

    Fix service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -9,7 +9,6 @@ var filesToCache = [
   '/points.json'
 ];
 
-/* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(cacheName).then(function (cache) {
@@ -18,11 +17,16 @@ self.addEventListener('install', function(e) {
   );
 });
 
-/* Serve cached content when offline */
 self.addEventListener('fetch', function(e) {
   e.respondWith(
-    fetch(e.request).catch(function() {
-      return caches.match(e.request);
+    caches.open(cacheName).then(function (cache) {
+      fetch(e.request).then(function (response) {
+        return cache.put(e.request, response.clone()).then(function () {
+          return response;
+        });
+      }).catch(function() {
+        return cache.match(e.request);
+      });
     })
   );
 });
