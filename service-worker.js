b1606f18c6cb7e5b9c50809a28daeb91cb1ebdfc
Zeev Rotshtein <zeevro@gmail.com>
2020-11-04 00:27:18 +0200

    Finally fixed service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'naviz-pwa';
+var cacheName = 'naviz-v1';
 var filesToCache = [
   '/',
   '/index.html',
@@ -6,27 +6,37 @@ var filesToCache = [
   '/style.css',
   '/bg.svg',
   '/arrow.svg',
-  '/points.json'
+  '/points.json',
+  '/GreatCircle.js'
 ];
 
 self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
+    caches.open(cacheName).then(function(cache) {
       cache.addAll(filesToCache)
     })
   );
 });
 
+// Try network and fall-back to cache (update cache if network succeeds) - This is good for debug or rapidly updating versions
 self.addEventListener('fetch', function(e) {
   e.respondWith(
-    caches.open(cacheName).then(function (cache) {
-      fetch(e.request).then(function (response) {
-        return cache.put(e.request, response.clone()).then(function () {
-          return response;
-        });
+    caches.open(cacheName).then(function(cache) {
+      return fetch(e.request).then(function(response) {
+        cache.put(e.request, response.clone());
+        return response;
       }).catch(function() {
         return cache.match(e.request);
       });
     })
   );
 });
+
+// Cache first with network fallback (not updating cache from network) - This is good for production since this is a static files app
+// self.addEventListener('fetch', function(e) {
+//   e.respondWith(
+//     caches.match(e.request).then(function(response) {
+//       return response || fetch(e.request);
+//     })
+//   );
+// });
